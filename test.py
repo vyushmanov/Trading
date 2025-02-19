2b34311310b10b4c96dbc3ff56e175f2e76c9c25
vyushmanov <mvyus@DESKTOP-74942MN.>
2025-02-19 23:36:00 +0300

    first api request

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-import pandas as pd
-
-print("Hellow, World!")
-
-data = {'col1': [1,2], 'col2': [3,4]}
-df = pd.DataFrame(data)
-
-print(df)
+from pybit.unified_trading import HTTP
+session = HTTP(testnet=True)
+print(session.get_kline(
+    category="linear",
+    symbol="BTCUSD",
+    interval=30,
+    start=1582070803000,
+    end=1582106803000,
+))
